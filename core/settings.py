95cd537bca56e5ad7ca46b84582f94a0c42d171b
mahdi <132130862+mwahdin@users.noreply.github.com>
2026-06-14 15:26:43 +0330

    remove extra files

--- core/settings.py
+++ core/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'blog.context_processors.top_authors_processor',
+                # 'blog.context_processors.top_authors_processor',
             ],
         },
     },
