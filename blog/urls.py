59c99d6b45ec03dcc1b768437ffbafa767706857
mahdi <132130862+mwahdin@users.noreply.github.com>
2026-06-21 06:08:36 +0330

    implement create edit delete post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import (PostListView, PostDetailView, AuthorView, AllCategoriesListView, CategoryPostListView)
+from .views import (PostListView, PostDetailView, AuthorView, AllCategoriesListView, CategoryPostListView, CreatePostView, PostUpdateView, PostDeleteView)
 
 app_name = 'blog' 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('post/create/', CreatePostView.as_view(), name='post_create'),
+    path('<slug:slug>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('<slug:slug>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('categories/', AllCategoriesListView.as_view(), name='all_categories'),
     path('category/<slug:slug>/', CategoryPostListView.as_view(), name='category_posts'),
     path('author/<str:username>/', AuthorView.as_view(), name='author_page'),
--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", HomeView.as_view(), name="main_page"),
     path("contact_us/", ContactUsView.as_view(), name="contact_page"),
     path("about_us/", AboutView.as_view(), name="about_page"),
+    path('comment/add/<int:post_id>', CommentCreateView.as_view(), name='add_comment'),
     path('comment/edit/<int:pk>/', CommentUpdateView.as_view(), name='comment_edit'),
 ]
