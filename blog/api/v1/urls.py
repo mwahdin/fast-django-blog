f3ee9cdb10ea5a1042070ee18a1c5b2e8ee39ecb
mwahdin <rahimzadehm65@gmail.com>
2026-07-10 23:55:19 +0330

    refactor: replace ModelViewSet with ViewSet for practice

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -1,13 +1,19 @@
 # urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import PostViewsett
 
 app_name = 'api-v1'
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+router.register(r'posts', PostViewsett, basename='post')
 
 urlpatterns = [
+    #modelViewSet
     path('', include(router.urls)),
+
+
+    #ViewSet
+    path('post/',PostViewsett.as_view({'get': 'list'}), name="post-list" ),
+    path('post/<int:pk>/',PostViewsett.as_view({'get': 'retrieve'}), name="post-list" ),
 ]
--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,10 +1,26 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets 
+from rest_framework.response import Response 
 from .Serializer import PostSerializer
 from ...models import Post
 
-
-class PostViewSet(ModelViewSet):
+#modelViewSet
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=Post.Status.PUBLISHED)
+
+#ViewSet
+class PostViewsett(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=Post.Status.PUBLISHED)
+    
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self,request, pk=None):
+        postObject = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_class(postObject)
+        return Response(serializer.data)
